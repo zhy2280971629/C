fecc60e420d5545b732c199498ed0b07147fd736
2280971629 <2280971629@qq.com>
2019-10-07 22:42:04 +0800

    用递归将一个十进制整数转化为所对应的十六进制整数

---
+++ 转化为十六进制/转化为十六进制/test.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+void Digit(int n)
+{
+	if (n<16)
+	{
+		switch (n)
+		{
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+			printf("%d", n);
+			break;
+		case 10:
+			printf("%c", 'A');
+			break;
+		case 11:
+			printf("%c", 'B');
+			break;
+		case 12:
+			printf("%c", 'C');
+			break;
+		case 13:
+			printf("%c", 'D');
+			break;
+		case 14:
+			printf("%c", 'E');
+			break;
+		case 15:
+			printf("%c", 'F');
+			break;
+		}
+	}
+	else
+	{
+		Digit(n / 16);
+		printf("%d", n % 16);
+	}
+}
+int main()
+{
+	int num = 0;
+	printf("input a integer:\n");
+	scanf("%d", &num);
+	Digit(num);
+	system("pause");
+	return 0;
+}
